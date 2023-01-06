021b5dd29a9a60cc12186e45faaca9b058bd49f1
Atharv <m.atharv07@gmail.com>
2023-01-06 16:11:04 +0530

    FTC PowerPlay: 2023 TeamCode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -33,7 +33,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double LATERAL_DISTANCE = 13.501790891783191; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -7.08; // in; offset of the lateral wheel
+    public static double FORWARD_OFFSET = -7.079; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 0.77871512; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 0.769477397; // Multiplier in the Y direction
