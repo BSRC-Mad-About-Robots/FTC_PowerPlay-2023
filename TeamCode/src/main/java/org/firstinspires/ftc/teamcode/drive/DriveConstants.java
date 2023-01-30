808c87d0e92a88f6a7f3c020724893b92db4fcf8
Anay Chauhan <anaychauhan1234@gmail.com>
2023-01-31 01:21:00 +0530

    TeamCode PowerPlay 2023

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -87,10 +87,10 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = 48;
-    public static double MAX_ACCEL = 48;
-    public static double MAX_ANG_VEL = 3.6;
-    public static double MAX_ANG_ACCEL = 3.6;
+    public static double MAX_VEL = 52;
+    public static double MAX_ACCEL = 52;
+    public static double MAX_ANG_VEL = 3.86;
+    public static double MAX_ANG_ACCEL = 3.86;
 
 
     public static double encoderTicksToInches(double ticks) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SplineTest.java
@@ -6,7 +6,9 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import java.lang.*;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 /*
  * This is an example of a more complex path to really test the tuning.
@@ -15,36 +17,47 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        long millis=System.currentTimeMillis();
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startPose = new Pose2d(32.5, -63, Math.toRadians(90));
+        drive.setPoseEstimate(startPose);
 
-        waitForStart();
-
-        if (isStopRequested()) return;
-        double x=0;
-        double y=0;
-        double q=0;
-
-//        q++;
-    drive.setPoseEstimate(new Pose2d(0, 0, drive.getRawExternalHeading()));
-        Trajectory traj = drive.trajectoryBuilder(new Pose2d(0,0))
-                .splineTo(new Vector2d(30, 30), 0)
+        Trajectory traj = drive.trajectoryBuilder(startPose)
+                .lineToSplineHeading(new Pose2d(34  , -12.1, Math.toRadians(0)))
                 .build();
 
-        drive.followTrajectory(traj);
-
-        sleep(4000);
-
-    Pose2d myPose = new Pose2d(30, 30, drive.getRawExternalHeading());
-    drive.setPoseEstimate(myPose);
-
-    drive.followTrajectory(
-                drive.trajectoryBuilder(traj.end(), true)
-                        .splineTo(new Vector2d(-1.9, 0.45), Math.toRadians(180))
-                        .build()
-        );
+        Trajectory traj3 = drive.trajectoryBuilder(new Pose2d())
+                .forward(7.5+11.7+23.4+7.5)
+                .build();
+        Trajectory traj2 = drive.trajectoryBuilder(startPose)
+                .forward(4)
+                .build();
+        Trajectory traj4 = drive.trajectoryBuilder(new Pose2d())
+                .forward(1.7+23.4)
+                .build();
+        Trajectory traj5 = drive.trajectoryBuilder(new Pose2d())
 //
+                .forward(39.25)
+//                .addSpatialMarker(new Vector2d(30, 0),() ->{})
+                .splineTo(new Vector2d(7.5+11.7+23.4+7.5+0.8, -4), Math.toRadians(-90))
+                .build();
+//
+        waitForStart();
+//clock.startmillis();
+//        if (isStopRequested()) return;
 
-    sleep(2000);
+//        Pose2d pose2d = new Pose2s
+
+
+        drive.followTrajectory(traj);
+telemetry.addData("time",System.currentTimeMillis()-millis);
+telemetry.update();
+//        sleep(100);
+//        drive.turn(Math.toRadians(-90));
+//        traj3.end();
+//        sleep(100);
+//        drive.setPoseEstimate(startPose);
+//        drive.followTrajectory(traj5);
 
 
     }
